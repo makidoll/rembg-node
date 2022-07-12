f9143a394c61312b81a7d86bf1286c9cc0c2dbd7
Maki <mxmcube@gmail.com>
2022-07-12 14:32:47 -0500

    Update README.md

--- README.md
+++ README.md
@@ -25,5 +25,8 @@ import sharp from "sharp";
 	const output = await rembg.remove(input);
 
 	await output.webp().toFile("test-output.webp");
+
+	// optionally you can use .trim() too!
+	await output.trim().webp().toFile("test-output-trimmed.webp");
 })();
 ```
--- test.mjs
+++ test.mjs
@@ -4,10 +4,15 @@ import sharp from "sharp";
 (async () => {
 	const input = sharp("test-input.jpg");
 
+	// optional arguments
 	const rembg = new RemBg({
 		logging: true,
 	});
 
 	const output = await rembg.remove(input);
+
 	await output.webp().toFile("test-output.webp");
+
+	// optionally you can use .trim() too!
+	await output.trim().webp().toFile("test-output-trimmed.webp");
 })();
